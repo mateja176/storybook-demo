1807328a49959d2825a5b35c39c6639f32f225f7
Mateja <mateja176@gmail.com>
2023-10-24 23:54:22 +0200

    adds storybook

---
+++ app/page.stories.tsx
@@ -0,0 +1,11 @@
+import Page from './page';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Page',
+  component: Page,
+} as Meta<{}>;
+
+type Story = StoryObj<typeof Page>;
+
+export const Default: Story = { args: {} };
